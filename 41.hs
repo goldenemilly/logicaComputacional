2cbffd4958e1f73f91420d998bb1b00e4fb9e65d
NaomiYatana <141897543+NaomiYatana@users.noreply.github.com>
2025-02-11 23:16:45 -0600

    Update 41.hs

--- 41.hs
+++ 41.hs
@@ -4,6 +4,7 @@ import qualified Distribution.SPDX as SPDX
 -- Tipo de dato llamado Shape que representa a las figuras geométricas.
 data Shape = Circle Float | Square Float | Rectangle Float Float | Triangle Float| Trapeze Float Float Float deriving (Show)
 
+--Función area: Calcula el área de una figura.
 area :: Shape -> Float
 area (Circle r) = pi * r^2
 area (Square l) = l^2
@@ -11,6 +12,7 @@ area (Rectangle b h) = b * h
 area (Triangle l) = (l^2) * sqrt 3 / 4
 area (Trapeze b1 b2 h) = (b1 + b2) * h / 2
 
+-- Función perimeter: Calcula el perimetro de una figura dada.
 perimeter :: Shape -> Float
 perimeter (Circle r) = 2 * pi * r
 perimeter (Square l) = 4 * l
@@ -23,16 +25,22 @@ instance Eq Shape where
 
 instance Ord Shape where
     compare s1 s2 = compare (area s1) (area s2)
-
+    
+-- Crea un tipo de dato llamado Point que representa un punto en el plano cartesiano.
 type Point = (Float, Float)
 distance :: Point -> Point -> Float
 distance (x1, y1) (x2, y2) = sqrt ((x2 - x1)^2 + (y2 - y1)^2)
 from0 :: Point -> Float
 from0 (x, y) = distance (0, 0) (x, y)
 
+-- Tipo de dato Haskellium que representa a un Haskellium.
 data Haskellium = Haskellium {nombre :: String, apellido1 :: String, apellido2 :: String, lugar :: Point, casa :: Shape} deriving (Show)
+
+-- Dados dos Haskellium y un String de nombre, regresa un Haskellium que sería hijo de los dos Haskelliums con el nombre dado.
 son :: Haskellium -> Haskellium -> String -> Haskellium
 son padre madre nombre1 = Haskellium {nombre = nombre1, apellido1 = apellido1 padre, apellido2 = apellido1 madre, lugar= lugar madre, casa = casa madre}
+
+-- 
 timeToWork :: Haskellium -> Float
 timeToWork dude = 
     let distance = from0  (lugar dude)
